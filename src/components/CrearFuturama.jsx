39f15f1fbccc8d39b2c88f90e9276074becaed9f
Liveribox <alvaroarella>
2024-03-29 00:16:47 +0100

    Mejorar interfaz mas aun

--- src/components/CrearFuturama.jsx
+++ src/components/CrearFuturama.jsx
@@ -56,49 +56,7 @@ export const CrearFuturama = ({addItem,newId,setOpenForm}) => {
         <br/>
         <Input placeholder="Enlace Imagen" id="image" value={inputImagen} onChange={(e) => setinputImagen(e.target.value)}/>
         
-      </Modal>
-
-        /*<>
-            <form>
-                <label htmlFor="name">Name:</label>
-                <input
-                    type="text"
-                    id="name"
-                    value={inputName}
-                    onChange={(e) => setInputName(e.target.value)}
-                />
-
-                <label htmlFor="gender">Gender:</label>
-                <input
-                    type="text"
-                    id="gender"
-                    value={inputGender}
-                    onChange={(e) => setInputGender(e.target.value)}
-                />
-
-                <label htmlFor="species">Species:</label>
-                <input
-                    type="text"
-                    id="species"
-                    value={inputSpecies}
-                    onChange={(e) => setInputSpecies(e.target.value)}
-                />
-
-                <label htmlFor="images">Species:</label>
-                <input
-                    type="text"
-                    id="images"
-                    value={inputImagen}
-                    onChange={(e) => setinputImagen(e.target.value)}
-                />
-
-                <button type="button" onClick={enviar}>Enviar</button>
-                
-            </form>
-        </>*/
-        
-
-            
+      </Modal>      
     )
 
     
